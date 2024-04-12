3f8b9ec6c7f9835aeba1f0c2a2b4b6a23b63aa22
Andrew Kwon <andrew_kwon@cs.ucsb.edu>
2024-04-11 18:42:21 -0700

    ucsb id

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Andrew Kwon!";
+        return "Greetings from andrew_kwon!";
     }
     
 }
